d126a85b97868420b8a3e31930d061c6e9ae6627
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-03-17 18:36:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(name: params[:username])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
   end
